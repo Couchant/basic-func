ceafdee5f99fc492d3f65731ebabc7f607973a62
Couchant <37212549+Couchant@users.noreply.github.com>
2020-08-29 11:21:10 +0800

    initial

---
+++ src/test/java/com/springcloud/basicfunc/BasicFuncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.basicfunc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFuncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
